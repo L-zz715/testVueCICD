f145a5f2c0212f26e8c4cb0035a543838dae3811
L-zz715 <luzhou1311@gmail.com>
2022-08-25 12:16:20 +0800

    testt

---
+++ testt.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# 保存当前目录
+# currentDir=$PWD
+# echo "Start to publish...\n"
+# 切换到项目目录
+#cd /var/www/html
+# 执行git命令
+# git pull origin main
+# git push origin master
+# 切换回原来的目录
+# cd $currentDir
+# 删除缓存 
+# rm -rvf /var/www/html/runtime/cache
+# echo "Success\n";
+# cd /home/git_dir
+output=`git pull origin main` > build.log
+echo "ret=" $?
